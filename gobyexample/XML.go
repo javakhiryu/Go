48e8cc12997d2aa821daa0edd6a1e849ab3175d4
javakhiryu <136607495+javakhiryu@users.noreply.github.com>
2024-07-16 00:37:41 +0500

    comments added

--- gobyexample/XML.go
+++ gobyexample/XML.go
@@ -1,3 +1,4 @@
+// Этот код на Go демонстрирует использование пакета encoding/xml для сериализации (маршалинга) и десериализации (анмаршалинга) структур в XML формат и из него.
 package main
 
 import (
@@ -5,43 +6,53 @@ import (
 	"fmt"
 )
 
+// Plant представляет собой структуру растения, которая будет сериализована в XML.
 type Plant struct {
-	XMLName xml.Name `xml:"plant`
-	Id      int      `xml:"id, attr"`
-	Name    string   `xml: "name"`
-	Origin  []string `xml:"origin"`
+	XMLName xml.Name `xml:"plant`     // Название XML-элемента, соответствующего этой структуре.
+	Id      int      `xml:"id, attr"` // Поле 'Id' будет представлено как атрибут XML.
+	Name    string   `xml: "name"`    // Поле 'Name' будет элементом XML.
+	Origin  []string `xml:"origin"`   // Поле 'Origin' будет элементом XML.
 }
 
+// Метод String для структуры Plant, который возвращает строковое представление структуры.
 func (p Plant) String() string {
 	return fmt.Sprintf("Plant id=%v, name=%v, origin=%v", p.Id, p.Name, p.Origin)
 }
 
 func main() {
+	// Создание объекта Plant.
 	coffee := &Plant{
 		Id:   1,
 		Name: "Coffee",
 	}
 	coffee.Origin = []string{"Ethiopia", "Brazil"}
+
+	// Сериализация объекта Plant в XML.
 	out, _ := xml.MarshalIndent(coffee, " ", " ")
 	fmt.Printf("%v\n", string(out))
 
+	// Десериализация XML обратно в объект Plant.
 	var p Plant
 	if err := xml.Unmarshal(out, &p); err != nil {
 		panic(err)
 	}
 	fmt.Println(p)
 
+	// Создание ещё одного объекта Plant.
 	tomato := &Plant{Id: 81, Name: "Tamoto"}
 	tomato.Origin = []string{"Mexico", "California"}
 
+	// Определение структуры, включающей вложенные элементы.
 	type Nexting struct {
 		XMLName xml.Name `xml:"nesting"`
 		Plants  []*Plant `xml:"parent>child>plant"`
 	}
 
+	// Создание объекта Nesting, содержащего два Plant.
 	nesting := &Nexting{}
 	nesting.Plants = []*Plant{coffee, tomato}
 
+	// Сериализация объекта Nesting в XML.
 	out, _ = xml.MarshalIndent(nesting, " ", " ")
 	fmt.Println(string(out))
 }
--- gobyexample/textTemplates.go
+++ gobyexample/textTemplates.go
@@ -6,32 +6,41 @@ import (
 )
 
 func main() {
+	// Создание нового шаблона t1 и его парсинг
 	t1 := template.New("t1")
 	t1, err := t1.Parse("Value is {{.}}\n")
 	if err != nil {
 		panic(err)
 	}
 
+	// Выполнение шаблона t1 с передачей строки "some text"
 	t1.Execute(os.Stdout, "some text")
 
+	// Функция для создания шаблона и обработки ошибок
 	Create := func(name, t string) *template.Template {
 		return template.Must(template.New(name).Parse(t))
 	}
 
+	// Создание и выполнение шаблона t2 с использованием структуры
 	t2 := Create("t2", "Name: {{.Name}}\n")
 
 	t2.Execute(os.Stdout, struct {
 		Name string
 	}{"Jane Doe"})
 
+	// Выполнение шаблона t2 с использованием карты
 	t2.Execute(os.Stdout, map[string]string{
 		"Name": "John Doe",
 	})
 
+	// Создание и выполнение шаблона t3 с условным оператором
 	t3 := Create("t3", "{{if . -}} Not empty: {{.}} {{else -}} empty {{end}}\n")
 	t3.Execute(os.Stdout, "")
 	t3.Execute(os.Stdout, "apple")
 
+	// Создание и выполнение шаблона t4 с циклом range
 	t4 := Create("t4", "Range: {{range .}}{{.}} {{end}}\n")
 	t4.Execute(os.Stdout, []string{"apple", "banana", "orange"})
 }
+
+//Этот пример демонстрирует мощные возможности шаблонов в Go, включая вставку значений, использование структур и карт, условные операторы и циклы. Шаблоны полезны для форматирования текста, генерации HTML и других сценариев, требующих динамического создания текстовых данных.
