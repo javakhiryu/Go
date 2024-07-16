5cee2399928d3860d09a2bffa0dadacb21e78abf
javakhiryu <136607495+javakhiryu@users.noreply.github.com>
2024-07-16 23:21:27 +0500

    comments added

--- gobyexample/commandLineArguments.go
+++ gobyexample/commandLineArguments.go
@@ -6,13 +6,22 @@ import (
 )
 
 func main() {
-	argsWithProg := os.Args
+	argsWithProg := os.Args // Получаем все аргументы, включая имя программы
 
-	argsWithoutProg := os.Args[1:]
 
+	argsWithoutProg := os.Args[1:] // Получаем все аргументы, кроме имени программы
+
+	// Получаем четвертый аргумент (индексация начинается с 0)
+	// Обратите внимание, что программа завершится ошибкой, если четвертый аргумент отсутствует.
 	arg := os.Args[3]
 
-	fmt.Println("Args with program name: ", argsWithProg)
-	fmt.Println("Args without progrma name: ", argsWithoutProg)
-	fmt.Println("4th argument: ", arg)
-}
+	fmt.Println("Args with program name: ", argsWithProg) // Выводим все аргументы, включая имя программы
+	fmt.Println("Args without progrma name: ", argsWithoutProg) // Выводим все аргументы, кроме имени программы
+	fmt.Println("4th argument: ", arg) // Выводим четвертый аргумент
+
+//go run main.go arg1 arg2 arg3 arg4 arg5
+
+//Args with program name:  [main.go arg1 arg2 arg3 arg4 arg5]
+//Args without program name:  [arg1 arg2 arg3 arg4 arg5]
+//4th argument:  arg4
+
--- gobyexample/latencyCheck.go
+++ gobyexample/latencyCheck.go
@@ -1,3 +1,5 @@
+//Этот пример демонстрирует, как параллельно отправлять HTTP-запросы и собирать результаты, используя горутины и каналы в Go. Основные компоненты программы включают тип данных result для хранения результатов запросов, функцию get для выполнения HTTP-запросов и основную функцию main, которая управляет запуском горутин и сбором результатов.
+
 package main
 
 import (
@@ -6,44 +8,49 @@ import (
 	"net/http"
 )
 
+// result представляет результат HTTP-запроса
 type result struct {
-	url     string
-	err     error
-	latency time.Duration
-}
+	url     string 			// URL, к которому был сделан запрос
+	err     error 			// Ошибка, если произошла
+	latency time.Duration 	// Время задержки запроса
 
+// get выполняет HTTP-запрос к указанному URL и отправляет результат в канал
 func get(url string, ch chan <- result){
-	start := time.Now()
+	start := time.Now() // Запоминаем время начала запроса
 
-	if resp, err := http.Get(url); err !=nil {
-		ch <- result {url, err, 0}
+	resp, err := http.Get(url) // Выполняем HTTP-запрос
+	if err !=nil {
+		ch <- result {url, err, 0} // Если произошла ошибка, отправляем результат с ошибкой и нулевой задержкой
 	} else{
-		t:= time.Since(start).Round(time.Millisecond)
-		ch <- result {url, nil, t}
-		resp.Body.Close()
+		t:= time.Since(start).Round(time.Millisecond) // Вычисляем время задержки
+		ch <- result {url, nil, t}   // Отправляем результат с задержкой
+		resp.Body.Close()   // Закрываем тело ответа
 	}
 }
 
 func main() {
-	results:=make(chan result)
-	list := []string{
+	results:=make(chan result) // Создаем канал для передачи результатов
+	list := []string{ // Список URL для запросов
+
 		"https://google.com",
 		"https://nytimes.com",
 		"https://amazon.com",
 		"https://wsj.com",
 
 	}
-	for _, url := range list{
+	for _, url := range list{ // Запускаем горутину для каждого URL
 		go get(url, results)
 	}
 
-	for range list{
-		r := <-results
+	for range list{ // Собираем результаты
+		r := <-results // Получаем результат из канала
 
-		if r.err !=nil{
+		if r.err !=nil{ // Если произошла ошибка
 			log.Printf("%-20s %s\n", r.url, r.err)
-		} else {
+		} else { // Если запрос успешен
 			log.Printf("%-20s %s\n", r.url, r.latency)
 		}
 	}
 }
+
+//Этот пример показывает, как эффективно использовать горутины и каналы для параллельного выполнения задач и сбора результатов.
