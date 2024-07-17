7432cd17cb05cd1be3fa6f487a159872524759a7
javakhiryu <136607495+javakhiryu@users.noreply.github.com>
2024-07-17 23:13:34 +0500

    some remarks added

--- gobyexample/latencyCheck/latencyCheck.go
+++ gobyexample/latencyCheck/latencyCheck.go
@@ -4,48 +4,48 @@ package main
 
 import (
 	"log"
-	"time"
 	"net/http"
+	"time"
 )
 
 // result представляет результат HTTP-запроса
 type result struct {
-	url     string 			// URL, к которому был сделан запрос
-	err     error 			// Ошибка, если произошла
-	latency time.Duration 	// Время задержки запроса
+	url     string        // URL, к которому был сделан запрос
+	err     error         // Ошибка, если произошла
+	latency time.Duration // Время задержки запроса
+}
 
 // get выполняет HTTP-запрос к указанному URL и отправляет результат в канал
-func get(url string, ch chan <- result){
+func get(url string, ch chan<- result) {
 	start := time.Now() // Запоминаем время начала запроса
 
 	resp, err := http.Get(url) // Выполняем HTTP-запрос
-	if err !=nil {
-		ch <- result {url, err, 0} // Если произошла ошибка, отправляем результат с ошибкой и нулевой задержкой
-	} else{
-		t:= time.Since(start).Round(time.Millisecond) // Вычисляем время задержки
-		ch <- result {url, nil, t}   // Отправляем результат с задержкой
-		resp.Body.Close()   // Закрываем тело ответа
+	if err != nil {
+		ch <- result{url, err, 0} // Если произошла ошибка, отправляем результат с ошибкой и нулевой задержкой
+	} else {
+		t := time.Since(start).Round(time.Millisecond) // Вычисляем время задержки
+		ch <- result{url, nil, t}                      // Отправляем результат с задержкой
+		resp.Body.Close()                              // Закрываем тело ответа
 	}
 }
 
 func main() {
-	results:=make(chan result) // Создаем канал для передачи результатов
-	list := []string{ // Список URL для запросов
+	results := make(chan result) // Создаем канал для передачи результатов
+	list := []string{            // Список URL для запросов
 
 		"https://google.com",
 		"https://nytimes.com",
 		"https://amazon.com",
 		"https://wsj.com",
-
 	}
-	for _, url := range list{ // Запускаем горутину для каждого URL
+	for _, url := range list { // Запускаем горутину для каждого URL
 		go get(url, results)
 	}
 
-	for range list{ // Собираем результаты
+	for range list { // Собираем результаты
 		r := <-results // Получаем результат из канала
 
-		if r.err !=nil{ // Если произошла ошибка
+		if r.err != nil { // Если произошла ошибка
 			log.Printf("%-20s %s\n", r.url, r.err)
 		} else { // Если запрос успешен
 			log.Printf("%-20s %s\n", r.url, r.latency)
--- gobyexample/spawningProcesses/spawningProcesses.go
+++ gobyexample/spawningProcesses/spawningProcesses.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-	"fmt"	// Пакет для форматированного вывода
-	"io"// Пакет для работы с вводом-выводом
-	"os/exec"// Пакет для выполнения внешних команд
+	"fmt"     // Пакет для форматированного вывода
+	"io"      // Пакет для работы с вводом-выводом
+	"os/exec" // Пакет для выполнения внешних команд
 )
 
 func main() {
 	// Создаем команду для выполнения команды "date"
 	dateCmd := exec.Command("date")
 
-	 // Выполняем команду и получаем её вывод
+	// Выполняем команду и получаем её вывод
 	dateOut, err := dateCmd.Output()
 	if err != nil {
-		panic(err)	// Если произошла ошибка, прерываем выполнение программы
+		panic(err) // Если произошла ошибка, прерываем выполнение программы
 	}
 
 	fmt.Println("> date")
-	fmt.Println(string(dateOut))  // Выводим результат команды "date"
+	fmt.Println(string(dateOut)) // Выводим результат команды "date"
 
-	 // Пробуем выполнить команду "date" с несуществующим флагом "-x"
+	// Пробуем выполнить команду "date" с несуществующим флагом "-x"
 	_, err = exec.Command("date", "-x").Output()
 	if err != nil {
 		// Обрабатываем ошибку в зависимости от её типа
@@ -35,7 +35,7 @@ func main() {
 		}
 	}
 
-// Создаем команду для выполнения команды "grep hello"
+	// Создаем команду для выполнения команды "grep hello"
 	grepCmd := exec.Command("grep", "hello")
 
 	// Получаем канал для записи ввода в команду "grep"
@@ -53,12 +53,12 @@ func main() {
 
 	// Читаем результат выполнения команды "grep"
 	grepBytes, _ := io.ReadAll(grepOut)
-	grepCmd.Wait()	// Ждем завершения команды
+	grepCmd.Wait() // Ждем завершения команды
 
 	fmt.Println("> grep hello")
 	fmt.Println(string(grepBytes)) // Выводим результат команды "grep"
 
-	 // Создаем команду для выполнения сложной команды "ls -a -l -h" через оболочку
+	// Создаем команду для выполнения сложной команды "ls -a -l -h" через оболочку
 	lsCmd := exec.Command("bash", "-c", "ls -a -l -h")
 	// Выполняем команду и получаем её вывод
 	lsOut, err := lsCmd.Output()
@@ -67,7 +67,7 @@ func main() {
 	}
 
 	fmt.Println("> ls -a -l -h")
-	fmt.Println(string(lsOut))	// Выводим результат команды "ls"
+	fmt.Println(string(lsOut)) // Выводим результат команды "ls"
 }
 
 //Выполнение команд оболочки из программы на Go может быть полезным в различных сценариях. Вот несколько примеров и причин, по которым это может понадобиться:
@@ -79,22 +79,22 @@ func main() {
 //Сборка проекта с помощью make.
 //Выполнение скриптов для развертывания и конфигурации серверов.
 
-package main
-
-import (
-    "fmt"
-    "os/exec"
-)
-
-func main() {
-    // Запуск команды ls для получения списка файлов в текущей директории
-    cmd := exec.Command("ls")
-    output, err := cmd.Output()
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println(string(output))
-}
+//package main
+//
+//import (
+//    "fmt"
+//    "os/exec"
+//)
+//
+//func main() {
+//    // Запуск команды ls для получения списка файлов в текущей директории
+//    cmd := exec.Command("ls")
+//    output, err := cmd.Output()
+//    if err != nil {
+//        panic(err)
+//    }
+//    fmt.Println(string(output))
+//}
 
 //2. Интеграция с существующими инструментами
 //Иногда полезно использовать уже существующие команды и инструменты, вместо того чтобы писать новый код для выполнения тех же задач. Это позволяет интегрировать вашу программу с другими утилитами и инструментами. Например:
@@ -103,27 +103,26 @@ func main() {
 //Запуск команды tar для архивации файлов.
 //Использование curl для выполнения HTTP-запросов.
 
-package main
-
-import (
-    "fmt"
-    "io"
-    "os/exec"
-)
-
-func main() {
-    // Использование команды grep для поиска строки в тексте
-    cmd := exec.Command("grep", "hello")
-    stdin, _ := cmd.StdinPipe()
-    stdout, _ := cmd.StdoutPipe()
-    cmd.Start()
-    io.WriteString(stdin, "hello world\nbye world")
-    stdin.Close()
-    output, _ := io.ReadAll(stdout)
-    cmd.Wait()
-    fmt.Println(string(output))
-}
-
+//package main
+//
+//import (
+//    "fmt"
+//    "io"
+//    "os/exec"
+//)
+//
+//func main() {
+//    // Использование команды grep для поиска строки в тексте
+//    cmd := exec.Command("grep", "hello")
+//    stdin, _ := cmd.StdinPipe()
+//    stdout, _ := cmd.StdoutPipe()
+//    cmd.Start()
+//    io.WriteString(stdin, "hello world\nbye world")
+//    stdin.Close()
+//    output, _ := io.ReadAll(stdout)
+//    cmd.Wait()
+//    fmt.Println(string(output))
+//}
 
 //3. Мониторинг и сбор метрик
 //Вы можете запускать команды для получения системной информации и метрик. Например:
@@ -131,23 +130,22 @@ func main() {
 //Запуск команды df для получения информации о свободном дисковом пространстве.
 //Использование top или ps для мониторинга использования ресурсов.
 
-package main
-
-import (
-    "fmt"
-    "os/exec"
-)
-
-func main() {
-    // Получение информации о свободном дисковом пространстве
-    cmd := exec.Command("df", "-h")
-    output, err := cmd.Output()
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println(string(output))
-}
-
+//package main
+//
+//import (
+//    "fmt"
+//    "os/exec"
+//)
+//
+//func main() {
+//    // Получение информации о свободном дисковом пространстве
+//    cmd := exec.Command("df", "-h")
+//    output, err := cmd.Output()
+//    if err != nil {
+//        panic(err)
+//    }
+//    fmt.Println(string(output))
+//}
 
 //4. Обработка пользовательских запросов
 //Программы могут выполнять команды оболочки в ответ на действия пользователя. Например:
@@ -155,25 +153,25 @@ func main() {
 //Запуск определенной команды при получении HTTP-запроса на веб-сервере.
 //Выполнение команды на основе ввода пользователя в интерфейсе командной строки.
 
-package main
-
-import (
-    "fmt"
-    "net/http"
-    "os/exec"
-)
-
-func handler(w http.ResponseWriter, r *http.Request) {
-    cmd := exec.Command("date")
-    output, err := cmd.Output()
-    if err != nil {
-        fmt.Fprintf(w, "Error: %v", err)
-        return
-    }
-    fmt.Fprintf(w, "Current date and time: %s", output)
-}
-
-func main() {
-    http.HandleFunc("/date", handler)
-    http.ListenAndServe(":8080", nil)
-}
+//package main
+//
+//import (
+//    "fmt"
+//    "net/http"
+//    "os/exec"
+//)
+//
+//func handler(w http.ResponseWriter, r *http.Request) {
+//    cmd := exec.Command("date")
+//    output, err := cmd.Output()
+//    if err != nil {
+//        fmt.Fprintf(w, "Error: %v", err)
+//        return
+//    }
+//    fmt.Fprintf(w, "Current date and time: %s", output)
+//}
+//
+//func main() {
+//    http.HandleFunc("/date", handler)
+//    http.ListenAndServe(":8080", nil)
+//}
